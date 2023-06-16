33bf4f1fa495f548d0c12e5225257e17c8f8fb9c
eisbetterthanpi <60023474+eisbetterthanpi@users.noreply.github.com>
2023-06-16 22:14:03 +0800

    Create data_ImageFolder.py

---
+++ data_ImageFolder.py
@@ -0,0 +1,24 @@
+# @title data (old)
+import torch
+import torchvision
+from torchvision import datasets, transforms
+import numpy as np
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+
+dir='/content/gsv70kg'
+
+data = datasets.ImageFolder(dir, transform=transform)
+# data = datasets.ImageFolder(dir, transform=None)
+torch.manual_seed(0)
+train_data, test_data = torch.utils.data.random_split(data, [.9,.1])
+
+batch_size = 16 # 64
+num_batches=int(np.ceil(len(train_data)/batch_size))
+
+# train_loader = torch.utils.data.DataLoader(train_data, sampler=train_sampler, batch_size=batch_size, num_workers=4, pin_memory=True)
+# test_loader = torch.utils.data.DataLoader(test_data, sampler=test_sampler, batch_size=batch_size, num_workers=4, pin_memory=True)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=4, pin_memory=True)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, num_workers=4, pin_memory=True)
+del data, train_data, test_data
+
